90e67a7847fb3eb95ae65a1b33e8db69e6c04a4b
Floorban <15696082812@163.com>
2024-06-16 15:53:30 +0200

    no switch anim no more

--- Assets/Scripts/FPSController/PlayerController.cs
+++ Assets/Scripts/FPSController/PlayerController.cs
@@ -140,18 +140,20 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButtonDown(1))
         {
             player.shootSelf = !player.shootSelf;
+            normalView.gameObject.SetActive(!player.shootSelf);
+            selfView.gameObject.SetActive(player.shootSelf);
 
-            if (player.shootSelf)
-            {
-                handAnimator.SetTrigger("ToSelf");
-            }
-            else
-            {
-                handAnimator.SetTrigger("ToNormal");
+            /*            if (player.shootSelf)
+                        {
+                            handAnimator.SetTrigger("ToSelf");
+                        }
+                        else
+                        {
+                            handAnimator.SetTrigger("ToNormal");
 
-                /*   normalView.gameObject.SetActive(false);
-                   selfView.gameObject.SetActive(true);*/
-            }
+                            //normalView.gameObject.SetActive(false);
+                            //selfView.gameObject.SetActive(true);
+                        }*/
         }
     }
 
