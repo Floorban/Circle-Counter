43c68fbfa1c92c1aeff316fe85fef25400252ec5
Floorban <15696082812@163.com>
2024-06-16 03:54:01 +0200

    tried trigger
    -still bug, press end door but game starts

--- Assets/Scripts/Gameplay/Door.cs
+++ Assets/Scripts/Gameplay/Door.cs
@@ -17,22 +17,27 @@ public class Door : MonoBehaviour, IInteractable
 
     public void OnInteract()
     {
-        if (timer != null)
-        {
-            if (!isStartDoor) 
-            {
-                timer.canRun = false;
-                Debug.Log("level ends");
-            }
-            else
-            {
-                timer.canRun = true;
-                Debug.Log("level starts");
-            }
-
-            timer.ShakeTimer();
-            player.transform.position = tpPoint.position;
-        }
+        Debug.Log("this is a door...");
     }
+    private void OnTriggerStay(Collider other)
+    {
 
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                if (!isStartDoor)
+                {
+                    timer.canRun = false;
+                    Debug.Log("level ends");
+                }
+                else
+                {
+                    timer.canRun = true;
+                    Debug.Log("level starts");
+                }
+
+                timer.ShakeTimer();
+                player.transform.position = tpPoint.position;
+            }
+        
+    }
 }
